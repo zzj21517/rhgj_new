0910429abe2fcd9e9b4c6e7f452c881ac4991e38
zhouyijun <18550408380@163.com>
2021-08-13 17:53:17 +0800

    调整

--- pages/member/member.js
+++ pages/member/member.js
@@ -1,4 +1,5 @@
 // pages/member/member.js
+const app = getApp()
 import {
   request
 } from '../../utils/request'
@@ -9,7 +10,8 @@ Page({
    */
   data: {
     active: 1,
-    memberComboList: []
+    memberComboList: [],
+    canUse: true, //提交二次防御
   },
 
   /**
@@ -17,6 +19,81 @@ Page({
    */
   onLoad: function (options) {
     this.reqMemberCombo()
+  },
+
+  // 立即开通
+  handleOpen() {
+    if (!this.data.canUse) {
+      return
+    }
+    //调用微信支付
+    //获取金额进行保证金充值
+    const uid = app.globalData.userInfo.rowGuid
+    const amt = this.data.memberComboList[this.data.active].price
+    if (!uid) {
+      return wx.showModal({
+        title: '提示',
+        content: '您目前没有登录，不能进行保证金充值',
+        showCancel: false
+      });
+    }
+    this.setData({
+      canUse: false
+    })
+    //请求支付页面
+    wx.login({
+      success: (res) => {
+        request('/WxPayController/PreOrder', {
+            money: amt,
+            content: "VIP会员开通",
+            uid: uid,
+            pid: uid,
+            code: res.code,
+            item: '09'
+          },
+          (data) => {
+            console.log(data, 'dd')
+            var payargs = data
+            wx.requestPayment({
+              timeStamp: String(payargs.timeStamp),
+              nonceStr: payargs.nonceStr,
+              package: payargs.package,
+              signType: payargs.signType,
+              paySign: payargs.paySign,
+              success: (res) => {
+                this.setData({
+                  canUse: true
+                })
+                //支付成功
+                wx.showModal({
+                  title: '提示',
+                  content: 'VIP会员开通成功',
+                  showCancel: false,
+                  success: function (res) {
+                    //设置账户金额
+                    wx.navigateTo({
+                      url: "/pages/my/my"
+                    })
+                  }
+                });
+              },
+              fail: (res) => {
+                console.log(res, 'rrr')
+                this.setData({
+                  canUse: true
+                })
+                wx.showModal({
+                  title: '提示',
+                  content: "支付失败，请重新支付。",
+                  showCancel: false,
+                  success: function (res) {}
+                })
+              }
+            })
+          }
+        )
+      }
+    });
   },
 
   //获取会员套餐列表
